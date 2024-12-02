62ca30adef780b726f4afa2a74273fab12da20c5
Renatta Schunemann <renattaschunemann@hotmail.com>
2024-12-02 20:01:33 -0300

    Commit na branch Bugs, alterações modulos Intelij

--- src/TeacherHelper.java
+++ src/TeacherHelper.java
@@ -1,12 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TeacherHelper {
 
-    public static final double horaAula = 17.50;
+    //  public static final double horaAula = 17.50;
     public static void main(String[] args) {
         int opcao=0, numeroAulas, qtdeAlunos, i;
         double salarioBase, horaAtividade, descansoSemanalRemunerado, salario, notaAluno, mediaAlunos;
-
+        double horaAula;
         Random gerador = new Random();
 
         Scanner leitor = new Scanner(System.in);
@@ -26,6 +27,8 @@ public class TeacherHelper {
                      * http://www1.sinprosp.org.br/guia_consultas.asp?mat=8*/
                     System.out.println("Para calcular seu salário base precisamos saber quantas aulas semanais o professor tem na instituição");
                     numeroAulas = leitor.nextInt();
+                    System.out.println("Qual o valor praticado pela escola:");
+                    horaAula = leitor.nextDouble()
                     salarioBase = numeroAulas * 4.5 * horaAula;
                     horaAtividade = salarioBase * 0.05;
                     descansoSemanalRemunerado = (salarioBase + horaAtividade) / 6;
